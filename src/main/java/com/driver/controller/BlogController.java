df11f0c2b6d9ca0e4b879bd51bec1834da135062
Suyash Kadam <suyash.kadam01@ad.infosys.com>
2023-08-19 16:26:51 +0530

    Testing

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -22,7 +22,8 @@ public class BlogController {
                                      @RequestParam String title,
                                      @RequestParam String content) {
         // Create a blog and add it under given user
-        return new ResponseEntity<>(blogService.createAndReturnBlog(userId, title, content),HttpStatus.CREATED);
+        blogService.createAndReturnBlog(userId, title, content);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{blogId}")
